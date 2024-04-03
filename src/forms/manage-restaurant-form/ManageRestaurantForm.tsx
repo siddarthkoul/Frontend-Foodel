081d01ee5b166e21abb8bcc4e35883c3bf75b153
Siddarth Koul <siddarthkoul21@gmail.com>
2024-04-03 20:42:07 +0530

    Manage Restaurant Page

---
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,69 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+
+const formSchema = z.object({
+  restaurantName: z.string({
+    required_error: "Restaurant name is required",
+  }),
+  city: z.string({
+    required_error: "City is required",
+  }),
+  country: z.string({
+    required_error: "Country is required",
+  }),
+  deliveryPrice: z.coerce.number({
+    required_error: "Delivery Price is required",
+    invalid_type_error: "must be a valid number",
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "Estimated Delivery time is required",
+    invalid_type_error: "must be a valid number",
+  }),
+  cuisines: z.array(z.string()).nonempty({
+    message: "please select at least one item",
+  }),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "name is required"),
+      price: z.coerce.number().min(1, "price is required"),
+    })
+  ),
+  imageUrl: z.string().optional(),
+  imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+type Props = {
+  onSave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+  const onSubmit = (formDataJson: restaurantFormData) => {
+    //TODO - convert formDataJson to a new FormData object
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
